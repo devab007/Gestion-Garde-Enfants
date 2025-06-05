14661b76070013a2639b061b4eaa35791f4a6798
devab007 <tallaabdoulwahab207@gmail.com>
2025-06-05 01:46:28 +0000

    Premier commit du projet python sur ma branche

---
+++ config.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:passer@localhost:5432/garderie_enfants'
+app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+#Test de la connexion avec la base de donnée
+if __name__=='__main__':
+  try:
+    with app.app_context():
+      connection = db.engine.connect()
+      print("Connexion a la base de donnees reussie!")
+      connection.close()
+  except Exception as e:
+    print("Erreur de connexion: ",e)
+
